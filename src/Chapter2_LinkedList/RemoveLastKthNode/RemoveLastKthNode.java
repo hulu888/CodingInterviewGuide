93adf047ece152661de0ce14b90a345bb1d53e38
keen <keen1@vip.qq.com>
2020-03-14 14:37:43 +0800

    CodingInterviewGuide

---
+++ src/Chapter2_LinkedList/RemoveLastKthNode/RemoveLastKthNode.java
@@ -0,0 +1,36 @@
+package Chapter2_LinkedList.RemoveLastKthNode;
+
+/**
+ * 【题目】
+ *      分别实现两个函数，一个可以删除单链表中倒数第K个几点，另一个可以删除双链表中倒数第K个节点
+ *
+ * 【要求】
+ *      如果链表长度为N，时间复杂度达到O(N),额外空间复杂度O(1)
+ */
+
+
+class Node{
+    int val;
+    Node next;
+    Node (int x){
+        this.val = x;
+        this.next = null;
+    }
+}
+
+
+public class RemoveLastKthNode {
+
+    public static Node removeSingleListLastKthNode(Node head){
+
+    }
+
+    public static Node removeDoubleListLastKthNode(Node head){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
